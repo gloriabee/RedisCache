54001bd05008ea9520c4f765eebb140f508891b4
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-12-03 12:15:03 +0700

    setup db connection and adding keys

---
+++ RedisCache/Program.cs
@@ -0,0 +1,19 @@
+﻿using RedisCache;
+
+var cache = RedisHelper.Database;
+//set key-value pair
+await cache.StringSetAsync("OURKEY", "GLORIA");
+//get value by key
+var value=await cache.StringGetAsync("OURKEY");
+
+for(int i=0;i<100;i++)
+{
+    await cache.StringSetAsync($"OURKEY_{i}",$"GLORIA_{i}");
+}
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine(await cache.StringGetAsync($"OURKEY_{i}"));
+}
+
+
---
+++ RedisCache/RedisHelper.cs
@@ -0,0 +1,29 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisCache
+{
+    internal class RedisHelper
+    {
+        private static IDatabase redisDatabase;
+        public static IDatabase Database
+        {
+            get
+            {
+                return redisDatabase;
+            }
+        }
+        static RedisHelper()
+        {
+            // creating connection
+            var connection = ConnectionMultiplexer.Connect("localhost:6379");
+            redisDatabase=connection.GetDatabase();
+
+
+        }
+    }
+}
